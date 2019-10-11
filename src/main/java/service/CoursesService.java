1604bad833f46ac01c94f3fdaac95cde03f41e9a
dokit0 <dek_93@abv.bg>
2019-10-11 19:08:26 +0300

    Initial commit

---
+++ src/main/java/service/CoursesService.java
@@ -0,0 +1,11 @@
+package service;
+
+import models.service.CourseServiceModel;
+
+import java.util.List;
+
+public interface CoursesService {
+    List<CourseServiceModel> getAllCourses();
+
+    void createCourse(String courseName);
+}
---
+++ src/main/java/service/implementations/CoursesServiceImpl.java
@@ -0,0 +1,43 @@
+package service.implementations;
+
+import models.entities.Course;
+import models.service.CourseServiceModel;
+import service.CoursesService;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CoursesServiceImpl implements CoursesService {
+    private final EntityManager entityManager;
+
+    @Inject
+    public CoursesServiceImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<CourseServiceModel> getAllCourses() {
+        return entityManager.createQuery("SELECT c FROM Course c", Course.class)
+                .getResultList()
+                .stream()
+                .map(course -> {
+                    CourseServiceModel model = new CourseServiceModel();
+                    model.setName(course.getName());
+                    model.setId(course.getId());
+                    return model;
+                })
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void createCourse(String name) {
+        entityManager.getTransaction().begin();
+        Course course = new Course();
+        course.setName(name);
+        entityManager.persist(course);
+
+        entityManager.getTransaction().commit();
+    }
+}
